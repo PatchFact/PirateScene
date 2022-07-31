6fac37919430847a562c827caf01428c2659dbed
nissanote <nissanote@gmail.com>
2022-07-31 03:05:08 -0400

    Added soundbanks and updated distance detections

--- PirateScene/Assets/Scripts/GameObjectDistance.cs
+++ PirateScene/Assets/Scripts/GameObjectDistance.cs
@@ -4,23 +4,61 @@ using UnityEngine;
 
 public class GameObjectDistance : MonoBehaviour
 {
+
+    public bool inTavern = false;
+
+    void Start()
+    {
+        AkSoundEngine.SetRTPCValue("tavern_LPF", 1f, GameObject.Find("WwiseGlobal")); //turn on in beginning  
+    }
+
     void Update()
     {
-        GameObject tavern = GameObject.FindGameObjectWithTag("Tavern");
+        GameObject tavern = GameObject.Find("wwise_tavern_trigger");
         GameObject crab = GameObject.FindGameObjectWithTag("DancingCrab");
         
         Vector3 position = transform.position;
 
         float tavernDistance = CalculateDistance(position, tavern);
         float crabDistance = CalculateDistance(position, crab);
-
-        AkSoundEngine.SetRTPCValue("tavern_distance", tavernDistance, GameObject.Find("WwiseGlobal"));
+      
         AkSoundEngine.SetRTPCValue("crab_distance", crabDistance, GameObject.Find("WwiseGlobal"));
+
+        Debug.LogWarning("Distance To Crab:" + crabDistance);
+
+        Debug.LogWarning("Distance To Tavern:" + tavernDistance);
+
+        if (!inTavern) { AkSoundEngine.SetRTPCValue("tavern_distance", tavernDistance, GameObject.Find("WwiseGlobal")); }
+
+        if (inTavern)
+        {
+            AkSoundEngine.SetRTPCValue("tavern_distance", 0f, GameObject.Find("WwiseGlobal")); //set to loudest            
+        }
     }
 
     float CalculateDistance(Vector3 playerPosition, GameObject target) 
     {
         Vector3 diff = target.transform.position - playerPosition;
         return diff.sqrMagnitude;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "tavern_check")
+        {
+            inTavern = true;
+            AkSoundEngine.SetRTPCValue("tavern_LPF", 0f, GameObject.Find("WwiseGlobal")); //turn LPF off
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "tavern_check")
+        {
+            inTavern = false;
+            AkSoundEngine.SetRTPCValue("tavern_LPF", 1f, GameObject.Find("WwiseGlobal")); //turn LPF on
+        }
+      
     }
 }
